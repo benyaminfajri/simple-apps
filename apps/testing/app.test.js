29068cd1f1d505943a7d346d7bd1d7253572e10c
benyamin <benyamin.fajri@gmail.com>
2025-08-13 14:43:02 +0700

    Balikin coba error di test

--- apps/testing/app.test.js
+++ apps/testing/app.test.js
@@ -8,7 +8,7 @@ const app = require('../app'); // Replace with the path to your application file
 describe('Unit Test /', () => {
   it('should respond with index.html', async () => {
     const response = await request(app).get('/');
-    expect(response.status).toBe(300);
+    expect(response.status).toBe(200);
   });
 });
 
